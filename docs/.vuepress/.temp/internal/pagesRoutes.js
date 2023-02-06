1c8a276cf55d64d55a8edf38c3a6dad00d4ca59f
piyushsinghania <piyushsinghania28@gmail.com>
2023-02-06 22:42:16 +0530

    Setupe formester docs in vue press 2

---
+++ docs/.vuepress/.temp/internal/pagesRoutes.js
@@ -0,0 +1,4 @@
+export const pagesRoutes = [
+  ["v-8daa1a0e","/",{"title":"Hello Formester"},["/index.html","/readme.md"]],
+  ["v-3706649a","/404.html",{"title":""},["/404"]],
+]
---
+++ docs/.vuepress/.temp/pages/index.html.vue
@@ -0,0 +1,4 @@
+<template><div><h1 id="hello-formester" tabindex="-1"><a class="header-anchor" href="#hello-formester" aria-hidden="true">#</a> Hello Formester</h1>
+</div></template>
+
+
