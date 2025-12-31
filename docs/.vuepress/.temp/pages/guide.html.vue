bc4bffc834650ae1f7edf15c893adb3733ac4d5b
abhay2133 <abhaybishthestudent@gmail.com>
2025-12-31 14:03:15 +0530

    remove temporary VuePress internal files and configurations

--- docs/.vuepress/.temp/pages/guide.html.vue
+++
@@ -1,5 +0,0 @@
-<template><div><h2 id="paypal-integration-with-formester" tabindex="-1"><a class="header-anchor" href="#paypal-integration-with-formester" aria-hidden="true">#</a> <a href="/paypal-integration">PayPal Integration with Formester</a></h2>
-<h2 id="introduction-to-formester-api" tabindex="-1"><a class="header-anchor" href="#introduction-to-formester-api" aria-hidden="true">#</a> <a href="/formester-api">Introduction to Formester API</a></h2>
-</div></template>
-
-
